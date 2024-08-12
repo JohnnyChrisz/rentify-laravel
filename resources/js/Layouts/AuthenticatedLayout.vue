16b2bab98432e8a98b0d2157e2e68f4e988140c4
hokiee <hokiee@hokiees-MacBook-Air.local>
2024-08-12 16:46:37 +0700

    update code

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -306,7 +306,9 @@ const sidebarOpen = ref(false);
           </div>
         </div>
       </div>
-
+      <!-- Toast alert -->
+      <Toast position="bottom-right"/>
+      <!-- Toast alert -->
       <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
         <div class="container px-6 py-8 mx-auto">
          
